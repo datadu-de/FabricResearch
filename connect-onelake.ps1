060dbac659cb30b2762d7155915a37b3a8f4e854
datadu-de <frank.wagner.1983@gmail.com>
2023-09-19 15:01:55 +0200

    initial commit

---
+++ connect-onelake.ps1
@@ -0,0 +1,42 @@
+#Install-Module Az.Storage -Repository PSGallery -Force
+
+Get-Content .env | ForEach-Object {
+    $name, $value = $_.split('=', 2)
+    if ([string]::IsNullOrWhiteSpace($name) || $name.Contains('#')) {
+        continue
+    }
+    Set-Content env:\$name $value
+}
+
+$TenantId = $env:TenantId
+$ApplicationId = $env:ApplicationId
+$AppSecret = $env:AppSecret
+
+$Fabric = @{
+    Workspace = 'FabricResearch'.ToLower()
+    Lakehouse = 'MDW'
+    Itemtype  = 'Lakehouse'
+    Folder    = 'somesubfolder/ODXtest'
+}
+
+$SrcFile = './testfile2.csv'
+
+
+###############################################
+
+$SecurePassword = ConvertTo-SecureString -String $AppSecret -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecurePassword
+
+Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $Credential
+
+$ctx = New-AzStorageContext `
+            -StorageAccountName 'onelake' `
+            -UseConnectedAccount `
+            -endpoint 'fabric.microsoft.com' 
+
+
+$DestFile = "$($Fabric.Lakehouse).$($Fabric.Itemtype)/Files/$($Fabric.Folder)/$($SrcFileName)"
+
+$SrcFileName = Split-Path $SrcFile -leaf
+
+New-AzDataLakeGen2Item -Context $ctx -FileSystem $($Fabric.Workspace.ToLower()) -Path $DestFile -Source $SrcFile -Force
